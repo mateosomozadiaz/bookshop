17963399acd6682a1457c5b18443497afd2552b0
Mateo Somoza Diaz <mateosomozadiaz@gmail.com>
2024-01-27 13:42:55 -0300

    Casi preentrega

--- js/script.js
+++ js/script.js
@@ -1,96 +1,79 @@
 const books = [
-    {id: 1, title: "Harry Potter y la Piedra Filosofal", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000},
-    {id: 2, title: "Harry Potter y la cámara secreta", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000},
-    {id: 3, title: "Harry Potter y el prisionero de Azkaban", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000},
-    {id: 4, title: "Harry Potter y el cáliz de fuego", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000},
-    {id: 5, title: "Harry Potter y la orden del Fénix", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000},
-    {id: 6, title: "Harry Potter y el misterio del príncipe", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000},
-    {id: 7, title: "Harry Potter y las reliquias de la muerte", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000},
-    {id: 8, title: "Percy Jackson y el ladrón del Rayo", author: "Rick Riordan", category: "Fantasía", genre: "Fantasía", price: 10000},
-    {id: 9, title: "Percy Jackson y el mar de los Monstruos", author: "Rick Riordan", category: "Fantasía", genre: "Fantasía", price: 10000},
-    {id: 10, title: "Percy Jackson y la maldición del Titán", author: "Rick Riordan", category: "Fantasía", genre: "Fantasía", price: 10000},
-    {id: 11, title: "Percy Jackson y la batalla del laberinto", author: "Rick Riordan", category: "Fantasía", genre: "Fantasía", price: 10000},
-    {id: 12, title: "Percy Jackson y el último Héroe del Olimpo", author: "Rick Riordan", category: "Fantasía", genre: "Fantasía", price: 10000},
-    {id: 13, title: "Percy Jackson y el Caliz de los Dioses", author: "Rick Riordan", category: "Fantasía", genre: "Fantasía", price: 10000},
-    {id: 13, title: "Los Juegos del Hambre", author: "Suzanne Collins", category: "Distopía", genre: "Ficción", price: 12500},
-    {id: 13, title: "En llamas", author: "Suzanne Collins", category: "Distopía", genre: "Ficción", price: 12500},
-    {id: 13, title: "Sinsajo", author: "Suzanne Collins", category: "Distopía", genre: "Ficción", price: 12500},
-    {id: 13, title: "Balada de los Pajaros Cantores", author: "Suzanne Collins", category: "Distopía", genre: "Ficción", price: 12500},
-]; /* Este array contiene objetos con claves que no se usan hasta ahora, pero que van a ser usadas en un futuro por una característica especial de la app. */
+    { title: "Harry Potter y la Piedra Filosofal", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000 },
+    { title: "Harry Potter y la cámara secreta", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000 },
+    { title: "Harry Potter y el prisionero de Azkaban", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000 },
+    { title: "Harry Potter y el cáliz de fuego", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000 },
+    { title: "Harry Potter y la orden del Fénix", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000 },
+    { title: "Harry Potter y el misterio del príncipe", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000 },
+    { title: "Harry Potter y las reliquias de la muerte", author: "J.K. Rowling", category: "Fantasía", genre: "Fantasía", price: 8000 },
+    { title: "Percy Jackson y el ladrón del Rayo", author: "Rick Riordan", category: "Fantasía", genre: "Fantasía", price: 10000 },
+    { title: "Percy Jackson y el mar de los Monstruos", author: "Rick Riordan", category: "Fantasía", genre: "Fantasía", price: 10000 },
+    { title: "Percy Jackson y la maldición del Titán", author: "Rick Riordan", category: "Fantasía", genre: "Fantasía", price: 10000 },
+    { title: "Percy Jackson y la batalla del laberinto", author: "Rick Riordan", category: "Fantasía", genre: "Fantasía", price: 10000 },
+    { title: "Percy Jackson y el último Héroe del Olimpo", author: "Rick Riordan", category: "Fantasía", genre: "Fantasía", price: 10000 },
+    { title: "Percy Jackson y el Caliz de los Dioses", author: "Rick Riordan", category: "Fantasía", genre: "Fantasía", price: 10000 },
+    { title: "Los Juegos del Hambre", author: "Suzanne Collins", category: "Distopía", genre: "Ficción", price: 12500 },
+    { title: "En llamas", author: "Suzanne Collins", category: "Distopía", genre: "Ficción", price: 12500 },
+    { title: "Sinsajo", author: "Suzanne Collins", category: "Distopía", genre: "Ficción", price: 12500 },
+    { title: "Balada de los Pajaros Cantores", author: "Suzanne Collins", category: "Distopía", genre: "Ficción", price: 12500 },
+]; /* Declara el array con los libros en stock */
+
+books.forEach(book => {
+    book.id = books.indexOf(book)
+    createBookItem(book);
+    // createBookItem(book)
+});
+/* Asigna al ID de cada libro su valor de index */
+
+function createBookItem(book) {
+    const bookItem = document.createElement("article");
+
+    bookItem.innerHTML = `<img src="img/books/${book.id}.png" alt="book cover"> <h6 class="book-title">${book.title}</h6> <p class="book-price">$${book.price}</p>`;
+
+    document.querySelector("#stock").append(bookItem);
+}
 
 const shoppingCart = [];
+/* Declara el array del carrito de compras */
 
-function showBookList() {
-    for (const book of books) {
-        console.log(book.id + " | " + book.title + " (Por " + book.author + ")");
-        /* Id | Titulo (Por Autor) */
+class shoppingCartItem {
+    constructor(id, title, author, amount, price) {
+        this.id = id
+        this.title = title
+        this.author = author
+        this.amount = amount
+        this.price = price
     }
 }
+/* Declara una clase que estructura los items del carrito de compras */
 
-function showShoppingCart() {
-    for (item of shoppingCart) {
-        console.table(shoppingCart, ["title", "author", "amount", "price"])
-    }
-}
+function addBookToCart(book) {
+    let inStock = books.some(stock => stock.id == book.id); /* Verifica si el libro está en la lista de stock */
+    let inCart = shoppingCart.some(item => item.id == book.id); /* Verifica si el libro ya fue agregado al carrito de compras */
 
-let keepAdding = true;
-function addBookToCart() {
+    if (inStock == true) {
 
-    let id = prompt("Ingrese el ID (número) correspondiente a el libro que desea agregar a el carrito de compras. Puede encontrar una lista con la información necesaria en la consola.");
+        if (inCart == true) {
 
-    let bookFound = false;
-    for (const book of books) {
-        if (parseInt(id) == book.id) {
-            bookFound = true;
-            let alreadyIncluded = false;
-            for (item of shoppingCart) {
-                if (item.id == book.id) {
-                    alreadyIncluded = true;
-                    item.amount++;
-                    item.price = item.price + book.price;
-                    break;
-                }
-            }
-            if (alreadyIncluded == false) {
-                shoppingCart.push({id: book.id, title: book.title, author: book.author, amount: 1, price: book.price});
-            }
-            alert(book.title + " ha sido agregado al carrito de compras.")
-            showShoppingCart();
-        }
-    }
-    if (bookFound == false) {
-        if (id == "") {
-            alert("No deje el espacio vacío.");
+            let foundItem = shoppingCart.find(item => item.id == book.id);
+            foundItem.amount++;
+            foundItem.price = foundItem.price + book.price;
+
         } else {
-            alert("Ingrese un Id (número) válido.");
+
+            shoppingCart.push(new shoppingCartItem(book.id, book.title, book.author, book.amount, book.price));
         }
-    }
-   
 
-    if (confirm("¿Deseas agregar otro libro al carrito de compras?") == true) {
-        keepAdding = true;
     } else {
-        keepAdding = false;
+        console.error("Invalid book argument");
     }
 }
 
-showBookList();
+const shoppingCartButton = document.querySelector("#shopping-cart-button")
+const shoppingCartScreen = document.querySelector("#shopping-cart")
 
-while (keepAdding == true) {
-    addBookToCart();
+function toggleShoppingCart() {
+    shoppingCartScreen.classList.toggle("hide")
 }
 
-/* Resumen:
-- Declara el array con los libros que existen en la tienda
-- Declara el array que guardará los elementos del carrito de compras
-- Declara una función que muestra una lista de todos los libros existentes en la consola
-- Declara una función que muestra el carrito de compras en la consola
-- Declara una función que:
-        - Pide al usuario un ID
-        - Verifica que el ID corresponda a uno de los libros (si no es así, alerta al usuario)
-        - Si ya se agregó el libro a la lista de compras anteriormente, se suma 1 unidad el la clave amount
-        - Si no, agrega un objeto nuevo al array 
-        - Confirma si el usuario quiere agregar otro elemento al carrito
-        - Muestra el carrito en la consola con la función
-- Muestra una lista de todos los libros existentes en la consola mediante la función dedicada a esa tarea
-- Ejecuta la función principal mientras el usuario confirme que quiere seguir agregando elementos al carrito */
+shoppingCartButton.addEventListener("click", toggleShoppingCart)
